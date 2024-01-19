afc31a3904a09c6b44e2ff9a1ee1e3470042f0fd
Sonal Singh <sonal1201kumar@gmail.com>
2024-01-20 00:15:37 +0530

    Day-3

---
+++ 2d_array Question/upper_tri.cpp
@@ -0,0 +1,76 @@
+// printing the lower trigaluar matrixx.........
+//  1 2 3 4
+//  0 6 7 8
+//  0 0 11 12
+//  0 0 0 16
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[4][4];
+//     int cnt = 1;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             arr[i][j] = cnt;
+//             cnt++;
+//         }
+//     }
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             if (i == j || i < j)
+//             {
+//             }
+//             else
+//             {
+//                 arr[i][j] = 0;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// ANOTHER VARIATION
+//  Q--> Check it is upper trianglar or Not;
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[4][4] = {{1, 2, 3, 4}, {0, 6, 7, 8}, {0, 0, 11, 12}, {0, 0, 0, 16}};
+    bool flag = 1;
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i > j)
+            {
+                if (arr[i][j] != 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+    }
+    if (flag)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
