470febddd833dec6d5a8795cc19c4d3d223aa883
Sonal Singh <116959671+sonal1201@users.noreply.github.com>
2024-01-19 17:55:27 +0530

    does_divide.cpp

--- Basic Question/does_divide.cpp
+++ Basic Question/does_divide.cpp
@@ -1,3 +1,9 @@
+// Does it divide Hackerearth
+// The sum of numbers from 1 to N, S=N*(N+1)/2 and Product P=1*2*3*..*N 
+// For P to be divisible by S, All we have to do is to check whether N+1 is prime or not, 
+// if it is prime then sum of the numbers will not divide product of the numbers
+// and if it is not prime then sum will divide product.
+
 #include <iostream>
 using namespace std;
 bool isPrime(int num)
