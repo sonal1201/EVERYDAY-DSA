d3fc792bd3458d43de64306826d2cd67c99c0bb0
Sonal Singh <sonal1201kumar@gmail.com>
2024-01-21 12:04:00 +0530

    Day-4

---
+++ 2d_array Question/row_with_max_1s.cpp
@@ -0,0 +1,10 @@
+// Row with max 1s
+// Input:
+// N = 4 , M = 4
+// Arr[][] = {{0, 1, 1, 1},
+//            {0, 0, 1, 1},
+//            {1, 1, 1, 1},
+//            {0, 0, 0, 0}}
+// Output: 2
+// Explanation: Row 2 contains 4 1's (0-based
+// indexing).
