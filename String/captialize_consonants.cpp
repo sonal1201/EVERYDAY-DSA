f346ede62ebd0c04923cdbed8543ea1df17b7a56
Sonal Singh <sonal1201kumar@gmail.com>
2024-01-22 22:54:59 +0530

    Dya-4

---
+++ String/captialize_consonants.cpp
@@ -0,0 +1,43 @@
+// TAKE WORD(STRING) INPUT FROM USER AND CAPTIALIZE ALL THE CONSONANTS
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string str;
+//     cin >> str;
+//     for (int i = 0; i < str.size(); i++)
+//     {
+//         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+//         {
+//             continue;
+//         }
+//         else
+//         {
+//             str[i] = toupper(str[i]);
+//         }
+//     }
+//     cout << str;
+//     return 0;
+// }
+
+// TAKE WORD(STRING) INPUT FROM USER AND CAPTIALIZE ALL THE VOWELS -> A,E,I,O,U
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+
+            // str[i] = toupper(str[i]);
+            str[i] = str[i] - 32; // without using function.
+        }
+    }
+    cout << str;
+    return 0;
+}
---
+++ String/syntax.cpp
@@ -0,0 +1,38 @@
+// SYNTAXXX......
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string city = "Ayodhya";
+//     cout << city << endl;
+//     string name;        // Another way to intialize
+//     getline(cin, name); // syntax to print the paragraph
+//     cout << name;
+//     return 0;
+// }
+
+// SOME FUNCTION....
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str = "Sonal singh";
+    cout << str.size() << endl; // Size
+    cout << str.substr(0, 5);
+}
+
+// String Conc and Adding........
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string str1 = "Sonal ";
+//     string str2 = "Singh";
+//     // cout << str1 + " " + str2;
+//     // else
+//     str1 += str2;
+//     cout << str1;
+// }
