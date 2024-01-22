ed009b8da79cf54045b005d434ee0fbf512c7c65
Sonal Singh <sonal1201kumar@gmail.com>
2024-01-22 11:54:52 +0530

    Day-5

---
+++ array Question/fib.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void fib(int arr[])
+{
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= 20; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+}
+void print(int arr[])
+{
+    for (int i = 0; i <= 20; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[20];
+    fib(arr);
+    print(arr);
+}
