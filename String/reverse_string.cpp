dd4a9d0d4e595669e5940532e8812e9145c52194
Sonal Singh <sonal1201kumar@gmail.com>
2024-01-24 17:48:51 +0530

    Day-7

---
+++ String/reverse_string.cpp
@@ -0,0 +1,19 @@
+// 344. Reverse String
+
+// apply Two pointer Algo
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int i = 0;
+        int j = s.size() - 1;
+        while (i < j)
+        {
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+    }
+};
