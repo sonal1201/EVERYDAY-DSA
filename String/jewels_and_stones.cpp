917d47f447dff5f32da3ed328e6b786741fef3ab
Sonal Singh <sonal1201kumar@gmail.com>
2024-01-25 22:27:57 +0530

    Day-8

---
+++ String/jewels_and_stones.cpp
@@ -0,0 +1,21 @@
+// LC-> Jewels and Stones
+
+class Solution
+{
+public:
+    int numJewelsInStones(string jewels, string stones)
+    {
+        int cnt = 0;
+        for (int i = 0; i < jewels.length(); i++)
+        {
+            for (int j = 0; j < stones.length(); j++)
+            {
+                if (jewels[i] == stones[j])
+                {
+                    cnt++;
+                }
+            }
+        }
+        return cnt++;
+    }
+};
