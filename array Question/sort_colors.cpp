034e35d30891471793aca35d15affab90a8cb94f
Sonal Singh <sonal1201kumar@gmail.com>
2024-01-30 19:34:13 +0530

    Day-12

---
+++ array Question/sort_colors.cpp
@@ -0,0 +1,30 @@
+// Sort Colors
+
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int i = 0;
+        int j = 0;
+        int k = nums.size() - 1;
+        while (j <= k)
+        {
+            if (nums[j] == 0)
+            {
+                swap(nums[i], nums[j]);
+                j++;
+                i++;
+            }
+            else if (nums[j] == 1)
+            {
+                j++;
+            }
+            else
+            {
+                swap(nums[j], nums[k]);
+                k--;
+            }
+        }
+    }
+};
