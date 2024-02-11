71a39a677321d6f8a68eb09d3608cc7a10c39bfc
Sonal Singh <sonal1201kumar@gmail.com>
2024-02-11 23:19:01 +0530

    Day-23

--- Sliding Window/maxi_subArray.cpp
+++ Sliding Window/maxi_subArray.cpp
@@ -20,3 +20,30 @@ public:
         return ans;
     }
 };
+
+// More OPTIMIZE OF SLIDING WINDOW.
+
+class Solution
+{
+public:
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        double maxi = INT_MIN;
+        double window_sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            window_sum += nums[i]; // SUM OF FIRST WINDOW
+        }
+        maxi = window_sum; // IF WE HAVE SUM FIRST TIME COPY THAT TO MAXI
+
+        for (int i = k; i < nums.size(); i++) // MOVING THE WINDOW FORWARD//
+        {
+            int first = i - k;     // SUBTRACT FIRST ELEMENT OF THE WINDOW
+            window_sum += nums[i]; // ADDING NEXT ELEMENT FROM THE LAST ELEMENT OF THE WINDOW...
+            window_sum -= nums[first];
+            maxi = max(maxi, window_sum); // FIND THE MAX
+        }
+        double ans = maxi / k;
+        return ans;
+    }
+};
