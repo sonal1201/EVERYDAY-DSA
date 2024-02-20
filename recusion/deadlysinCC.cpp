95ffd96c8aabeb21961c154e7be4d685abd4463c
Sonal Singh <sonal1201kumar@gmail.com>
2024-02-20 18:55:26 +0530

    Day-32

---
+++ recusion/deadlysinCC.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int deadly(int m, int b)
+{
+    if (m == b)
+        return m + b;
+    else if (m > b)
+        deadly(m - b, b);
+    else if (m < b)
+        deadly(m, b - m);
+}
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int m, b;
+        cin >> m >> b;
+        cout << deadly(m, b) << endl;
+    }
+}
