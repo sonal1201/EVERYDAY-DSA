8edfd65d9e9b58f153ffe5ebb3a980ea7b755ea3
Sonal Singh <sonal1201kumar@gmail.com>
2024-03-13 20:24:13 +0530

    Day-46

---
+++ Linked list/reverse_SLL.cpp
@@ -0,0 +1,29 @@
+// REVERSE SINGLY LL;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *temp = head;
+        ListNode *prev = NULL;
+        while (temp != NULL)
+        {
+            ListNode *front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+};
