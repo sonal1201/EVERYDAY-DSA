6da03247fe3ddc4a20baa20750e53ee79a5d8685
Sonal Singh <sonal1201kumar@gmail.com>
2024-03-30 23:34:35 +0530

    Day-57

---
+++ hashTable/nextGreaterEle.cpp
@@ -0,0 +1,24 @@
+// NEXT GREATER ELEMENT - 1
+
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &findNums, vector<int> &nums)
+    {
+        stack<int> s;
+        unordered_map<int, int> m;
+        for (int n : nums)
+        {
+            while (s.size() && s.top() < n)
+            {
+                m[s.top()] = n;
+                s.pop();
+            }
+            s.push(n);
+        }
+        vector<int> ans;
+        for (int n : findNums)
+            ans.push_back(m.count(n) ? m[n] : -1);
+        return ans;
+    }
+};
