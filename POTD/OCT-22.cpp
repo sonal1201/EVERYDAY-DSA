c281529fe6646e0081858c11f5f77004fc599969
Sonal Singh <sonal1201kumar@gmail.com>
2024-10-22 19:29:23 +0530

    KlargestSumInBT

---
+++ POTD/OCT-22.cpp
@@ -0,0 +1,38 @@
+// Kth Largest Sum in a Binary Tree
+
+class Solution
+{
+public:
+    long kthLargestLevelSum(TreeNode *root, int k)
+    {
+        if (!root)
+            return -1;
+        priority_queue<long, vector<long>, greater<long>> pq;
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int size = q.size();
+            long sum = 0;
+            while (size > 0)
+            {
+                TreeNode *temp = q.front();
+                q.pop();
+                sum += temp->val;
+
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+                size--;
+            }
+
+            pq.push(sum);
+
+            if (pq.size() > k)
+                pq.pop();
+        }
+        return pq.size() < k ? -1 : pq.top();
+    }
+};
